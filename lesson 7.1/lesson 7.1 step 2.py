c19c861fde6946278654b241204091187ffc3370
Сидоров Егор <sidorov_ea@niichimmash.local>
2023-04-20 12:58:43 +0300

    create echo bot

---
+++ lesson 7.1/lesson 7.1 step 2.py
@@ -0,0 +1,37 @@
+from aiogram import Bot, Dispatcher
+from aiogram.dispatcher.filters import Command
+from aiogram.types import Message
+from aiogram.utils import executor
+from TOKEN import TOKEN
+
+
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
+API_TOKEN: str = TOKEN
+
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(token=API_TOKEN)
+dp: Dispatcher = Dispatcher(bot)
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message_handler(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message_handler(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer('Напиши мне что-нибудь и в ответ '
+                         'я пришлю тебе твое сообщение')
+
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message_handler()
+async def send_echo(message: Message):
+    await message.reply(text=f"Давай что-нибудь умное, а не {message.text}")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
