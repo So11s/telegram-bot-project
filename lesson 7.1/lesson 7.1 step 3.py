78c7cf911168aa8b73325b1c0a6af8a69b60edd7
Сидоров Егор <sidorov_ea@niichimmash.local>
2023-04-20 14:31:40 +0300

    add possibly get echo for audio and stickers

--- lesson 7.1/lesson 7.1 step 3.py
+++ lesson 7.1/lesson 7.1 step 3.py
@@ -1,6 +1,5 @@
 from aiogram import Bot, Dispatcher
-from aiogram.dispatcher.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, ContentType
 from aiogram.utils import executor
 from TOKEN import TOKEN
 
@@ -15,23 +14,41 @@ dp: Dispatcher = Dispatcher(bot)
 
 # Этот хэндлер будет срабатывать на команду "/start"
 async def process_start_command(message: Message):
-    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+    await message.reply('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
 async def process_help_command(message: Message):
-    await message.answer('Напиши мне что-нибудь и в ответ '
+    await message.reply('Напиши мне что-нибудь и в ответ '
                          'я пришлю тебе твое сообщение')
+
+
+# Этот хэндлер будет срабатывать на фотографии
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
+
+
+# Этот хендлер будет срабатывать на аудио
+async def send_audio_echo(message: Message):
+    await message.reply_audio(message.audio.file_id)
+
+
+# Это хендлер будет срабатывать на стикер
+async def send_sticker_echo(message: Message):
+    await message.reply_sticker(message.sticker.file_id)
 
 
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
 async def send_echo(message: Message):
-    await message.reply(text=f"Давай что-нибудь умное, а не {message.text}")
+    await message.reply(text=f"Давай что-нибудь умное, а не '{message.text}'")
 
 
 dp.register_message_handler(process_start_command, commands=['start'])
 dp.register_message_handler(process_help_command, commands=['help'])
+dp.register_message_handler(send_photo_echo, content_types=['photo'])
+dp.register_message_handler(send_audio_echo, content_types=['audio'])
+dp.register_message_handler(send_sticker_echo, content_types=['sticker'])
 dp.register_message_handler(send_echo)
 
 
