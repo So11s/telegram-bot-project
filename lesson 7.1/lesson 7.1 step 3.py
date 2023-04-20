da3d7cd2a26c794b022f854ddf5141751eb199d4
Сидоров Егор <sidorov_ea@niichimmash.local>
2023-04-20 13:22:20 +0300

    add echo bot with registratin handler

--- lesson 7.1/lesson 7.1 step 3.py
+++ lesson 7.1/lesson 7.1 step 3.py
@@ -1,17 +1,16 @@
-"""This code work on aiogram library ver. >3"""
-
 from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
+from aiogram.dispatcher.filters import Command
 from aiogram.types import Message
+from aiogram.utils import executor
 from TOKEN import TOKEN
 
-# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
-# полученный у @BotFather
+
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
 API_TOKEN: str = TOKEN
 
 # Создаем объекты бота и диспетчера
 bot: Bot = Bot(token=API_TOKEN)
-dp: Dispatcher = Dispatcher()
+dp: Dispatcher = Dispatcher(bot)
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
@@ -28,13 +27,13 @@ async def process_help_command(message: Message):
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    await message.reply(text=f"Давай что-нибудь умное, а не {message.text}")
 
 
-# Регистрируем хэндлеры
-dp.message.register(process_start_command, Command(commands=["start"]))
-dp.message.register(process_help_command, Command(commands=['help']))
-dp.message.register(send_echo)
+dp.register_message_handler(process_start_command, commands=['start'])
+dp.register_message_handler(process_help_command, commands=['help'])
+dp.register_message_handler(send_echo)
+
 
 if __name__ == '__main__':
-    dp.run_polling(bot)
+    executor.start_polling(dp)
