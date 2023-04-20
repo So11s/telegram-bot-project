d81a1458054de13b404b10c209651412bcbe9364
Сидоров Егор <sidorov_ea@niichimmash.local>
2023-04-20 14:58:20 +0300

    create univeral echo bot, but he dont working

--- lesson 7.1/lesson 7.1 step 6.py
+++ lesson 7.1/lesson 7.1 step 6.py
@@ -13,43 +13,26 @@ dp: Dispatcher = Dispatcher(bot)
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
+@dp.message_handler(commands=['start'])
 async def process_start_command(message: Message):
     await message.reply('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
+@dp.message_handler(commands=['help'])
 async def process_help_command(message: Message):
     await message.reply('Напиши мне что-нибудь и в ответ '
                          'я пришлю тебе твое сообщение')
 
 
-# Этот хэндлер будет срабатывать на фотографии
-async def send_photo_echo(message: Message):
-    await message.reply_photo(message.photo[0].file_id)
-
-
-# Этот хендлер будет срабатывать на аудио
-async def send_audio_echo(message: Message):
-    await message.reply_audio(message.audio.file_id)
-
-
-# Это хендлер будет срабатывать на стикер
-async def send_sticker_echo(message: Message):
-    await message.reply_sticker(message.sticker.file_id)
-
-
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
+@dp.message_handler()
 async def send_echo(message: Message):
-    await message.reply(text=f"Давай что-нибудь умное, а не '{message.text}'")
-
-
-dp.register_message_handler(process_start_command, commands=['start'])
-dp.register_message_handler(process_help_command, commands=['help'])
-dp.register_message_handler(send_photo_echo, content_types=['photo'])
-dp.register_message_handler(send_audio_echo, content_types=['audio'])
-dp.register_message_handler(send_sticker_echo, content_types=['sticker'])
-dp.register_message_handler(send_echo)
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text=f"Данный тип апдейтов не поддерживается методом send_copy")
 
 
 if __name__ == '__main__':
