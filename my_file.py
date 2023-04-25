146ca86f5167d7db2d78f608386d928ffe9afac5
Сидоров Егор <sidorov_ea@niichimmash.local>
2023-04-25 12:31:40 +0300

    add config file for new bot

--- my_file.py
+++ my_file.py
@@ -1,24 +1,32 @@
 from aiogram.dispatcher.filters import BoundFilter
 from aiogram.types import Message
 from typing import List
+from environs import Env
 
-def custom_filter(lst):
-    new_lst = filter(lambda n: type(n) == int and n % 7 == 0, lst)
-    return sum(new_lst) <= 83
+env = Env()
+env.read_env()
 
-some_list = [7, 14, 28, 32, 32, 56]
+bot_token = env("BOT_TOKEN")
+print(bot_token)
 
+# def custom_filter(lst):
+#     new_lst = filter(lambda n: type(n) == int and n % 7 == 0, lst)
+#     return sum(new_lst) <= 83
+#
+# some_list = [7, 14, 28, 32, 32, 56]
+#
+#
+# anonymous_filter = lambda s: s.lower().count('я') >= 23
+# # print(anonymous_filter('яяяяяяяяяяяяяяяяяяяяяяяя, яяяяяяяяяяяяяяяя и яяяяяяяя тоже!'))
+#
+# admin_ids: List[int] = [173901673, 178876776, 197177271]
+#
+# class IsAdmin(BoundFilter):
+#     def __init__(self, admin_ids: List[int]) -> None:
+#         self.admin_ids = admin_ids
+#
+#     async def __call__(self, message: Message,*args, **kwargs):
+#         return message.from_user.id in self.admin_ids
+#
+# # print(type(admin_ids))
 
-anonymous_filter = lambda s: s.lower().count('я') >= 23
-# print(anonymous_filter('яяяяяяяяяяяяяяяяяяяяяяяя, яяяяяяяяяяяяяяяя и яяяяяяяя тоже!'))
-
-admin_ids: List[int] = [173901673, 178876776, 197177271]
-
-class IsAdmin(BoundFilter):
-    def __init__(self, admin_ids: list[int]) -> None:
-        self.admin_ids = admin_ids
-
-    async def __call__(self, message: Message,*args, **kwargs):
-        return message.from_user.id in self.admin_ids
-
-print(type(admin_ids))
